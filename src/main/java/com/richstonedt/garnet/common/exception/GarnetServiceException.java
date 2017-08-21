7dab534dfcc7bb6ccd459a13799b8ed5ae607878
陈泽潮 <chenzechao@richstonedt.com>
2017-08-21 17:16:57 +0800

    Feat/add about exception

---
+++ src/main/java/com/richstonedt/garnet/common/exception/GarnetServiceException.java
@@ -0,0 +1,124 @@
+/*
+ * 广州丰石科技有限公司拥有本软件版权2017并保留所有权利。
+ * Copyright 2017, Guangzhou Rich Stone Data Technologies Company Limited,
+ * All rights reserved.
+ */
+
+package com.richstonedt.garnet.common.exception;
+
+/**
+ * <b><code>LteDetectiveServiceException</code></b>
+ * <p/>
+ * lte-detective Service Exception
+ * <p/>
+ * <b>Creation Time:</b> 2016年11月28日 下午5:24:39
+ *
+ * @author chenzechao
+ * @since garnet 1.0.0
+ */
+public class GarnetServiceException extends RuntimeException {
+
+    /**
+     * Serialization ID
+     *
+     * @since garnet 1.0.0
+     */
+    private static final long serialVersionUID = 3894491214688661572L;
+
+    /**
+     * Error code
+     *
+     * @since garnet 1.0.0
+     */
+    private String errorCode = GarnetServiceErrorCodes.OTHER;
+
+    /**
+     * Instantiates a new Garnet service exception.
+     *
+     * @param message the message
+     * @since garnet 1.0.0
+     */
+    public GarnetServiceException(String message) {
+        super(message);
+    }
+
+    /**
+     * Instantiates a new Garnet service exception.
+     *
+     * @param message   the message
+     * @param errorCode the error code
+     * @since garnet 1.0.0
+     */
+    public GarnetServiceException(String message, String errorCode) {
+        super(message);
+        setErrorCode(errorCode);
+    }
+
+    /**
+     * Instantiates a new Garnet service exception.
+     *
+     * @param message the message
+     * @param cause   the cause
+     * @since garnet 1.0.0
+     */
+    public GarnetServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Instantiates a new Garnet service exception.
+     *
+     * @param message   the message
+     * @param cause     the cause
+     * @param errorCode the error code
+     * @since garnet 1.0.0
+     */
+    public GarnetServiceException(String message, Throwable cause,
+                                  String errorCode) {
+        super(message, cause);
+        setErrorCode(errorCode);
+    }
+
+    /**
+     * Instantiates a new Garnet service exception.
+     *
+     * @param cause the cause
+     * @since garnet 1.0.0
+     */
+    public GarnetServiceException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * Instantiates a new Garnet service exception.
+     *
+     * @param cause     the cause
+     * @param errorCode the error code
+     * @since garnet 1.0.0
+     */
+    public GarnetServiceException(Throwable cause, String errorCode) {
+        super(cause);
+        setErrorCode(errorCode);
+    }
+
+    /**
+     * Returns the errorCode
+     *
+     * @return the errorCode
+     * @since garnet 1.0.0
+     */
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    /**
+     * Sets the errorCode
+     *
+     * @param errorCode the error code
+     * @since garnet 1.0.0
+     */
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+}
