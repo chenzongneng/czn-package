2912e60a19fbf24d06bb451030678b73712bc903
陈泽潮 <chenzechao@richstonedt.com>
2017-08-23 17:37:39 +0800

    Feat/add sql of Tenant

--- SQL/db_init_postgre.sql
+++ SQL/db_init_postgre.sql
@@ -1,6 +1,6 @@
--- ----------------------------
+-- ---------------------------------------------------------------------------------------------------------------------
 -- Table gar_departments
--- ----------------------------
+-- ---------------------------------------------------------------------------------------------------------------------
 DROP TABLE IF EXISTS "public"."gar_departments";
 CREATE TABLE "public"."gar_departments" (
   "id"          SERIAL,
@@ -21,3 +21,19 @@ COMMENT ON COLUMN "public"."gar_departments"."delete_flag" IS '是否删除[fals
 -- Records
 INSERT INTO "public"."gar_departments" (id, name, parent_id, tenant_id, order_num, delete_flag)
 VALUES ('0', '无', NULL, NULL, '0', 'f');
+
+-- ---------------------------------------------------------------------------------------------------------------------
+-- Table gar_tenants
+-- ---------------------------------------------------------------------------------------------------------------------
+DROP TABLE IF EXISTS "public"."gar_tenants";
+CREATE TABLE "public"."gar_tenants" (
+  "id"          SERIAL,
+  "name"        VARCHAR(50) COLLATE "default",
+  "delete_flag" BOOL DEFAULT FALSE
+) WITH (OIDS = FALSE
+);
+ALTER TABLE "public"."gar_tenants"
+  ADD PRIMARY KEY ("id");
+COMMENT ON COLUMN "public"."gar_tenants"."name" IS '租户名称';
+COMMENT ON COLUMN "public"."gar_tenants"."delete_flag" IS '是否删除[false:正常; true:已删除]';
+
